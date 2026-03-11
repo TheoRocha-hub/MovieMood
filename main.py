f420dbbad5d51ca6901aaf4d39f869b3a6cc7e43
TheoRocha-hub <theoprocha@gmail.com>
2026-03-11 11:56:27 -0300

    add main API file

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+import httpx
+
+app = FastAPI()
+API_KEY = "2f43ac16"
+
+@app.get("/")
+def home():
+    return {"message": "MovieMood API funcionando"}
+
+@app.get("/buscar filme")
+async def buscar_filme(titulo: str):
+    url = "http://www.omdbapi.com/"
+
+    params = {
+        "apikey": API_KEY,
+        "s": titulo
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, params=params)
+
+    dados = response.json()
+
+    filmes = []
+    for filme in dados["Search"]:
+        filmes.append({
+            "titulo" : filme["Title"],
+            "ano" : filme["Year"],
+            "poster" : filme["Poster"],
+            "imdb_id": filme["imdbID"]
+        })
+    return filmes
